b652cd21e90f72f81a627fd2734277552aa7d89d
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-03-22 10:53:54 +0200

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace SoftUniBarIncome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string pattern = @"^[^|$%.]*%(?<customer>[A-Z][a-z]+)%[^|$%.]*<(?<product>\w+)>[^|$%.]*\|(?<qtity>\d+)\|[^|$%.]*?(?<price>\d+\.?\d*)[^|$%.]*\$";
+
+            Regex regex = new Regex(pattern);
+            double income = 0;
+
+            while (true)
+            {
+                string line = Console.ReadLine();          //customer, product, count and a price:
+
+                if (line == "end of shift")
+                {
+                    break;
+                }
+
+                Match match = regex.Match(line);
+
+                if (match.Success == false)
+                {
+                    continue;
+                }
+
+                string customer = match.Groups["customer"].Value;
+                string product = match.Groups["product"].Value;
+                int quantity = int.Parse(match.Groups["qtity"].Value);
+                double price = (double.Parse)(match.Groups["price"].Value);
+
+                double totalCustomerIncome = price * quantity;
+                income += totalCustomerIncome;
+
+                Console.WriteLine($"{customer}: {product} - {totalCustomerIncome:F2}");               
+ 
+            }
+
+            Console.WriteLine($"Total income: {income:F2}");
+        }
+    }
+}
